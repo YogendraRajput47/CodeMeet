f3d760201652d339b6b0716149f056e1010cb32b
Yogendra Rajput <yogendra.leetcode123@gmail.com>
2025-11-28 19:15:05 +0530

    updating protectroute.js

--- backend/src/middleware/protectRoute.js
+++ backend/src/middleware/protectRoute.js
@@ -7,11 +7,11 @@ export const protectRoute = [
     try {
       const clerkId = req.auth().userId;
       if (!clerkId)
-        return res.status(401).json({ msg: "Unauthorized - Invalid Token" });
+        return res.status(401).json({ message: "Unauthorized - Invalid Token" });
       // find the user in db by clerkId
       const user = await User.findOne({ clerkId });
       if (!user) {
-        return res.status(404).json({ msg: "User not found" });
+        return res.status(404).json({ message: "User not found" });
       }
 
       //attach user to req object
@@ -19,7 +19,7 @@ export const protectRoute = [
       next();
     } catch (error) {
       console.error("Error in protectRoute middleware", error);
-      return res.status(500).json({ msg: "Internal server error" });
+      return res.status(500).json({ message: "Internal server error" });
     }
   },
 ];
