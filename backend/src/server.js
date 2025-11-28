3ba9f2480f4b3b129c717c832417e8b53bf5c0fd
Yogendra Rajput <yogendra.leetcode123@gmail.com>
2025-11-28 15:39:07 +0530

    db and tools setup

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,9 +1,10 @@
 import express from "express";
 import { ENV } from "./lib/env.js";
-import path from "path"
+import path from "path";
+import { connectDB } from "./lib/db.js";
 const app = express();
 
-const __dirname=path.resolve();
+const __dirname = path.resolve();
 
 app.get("/health", (req, res) => {
   res.status(200).json({ message: "Server is running" });
@@ -13,19 +14,23 @@ app.get("/books", (req, res) => {
 });
 
 // make our app ready for deployemnt
-if(ENV.NODE_ENV==="production"){
-  app.use(express.static(path.join(__dirname,"../frontend/dist")));
+if (ENV.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
-  app.get("/{*any}",(req,res)=>{
-    res.sendFile(path.join(__dirname,"../frontend","dist","index.html"))
+  app.get("/{*any}", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
   });
 }
 
-app.listen(ENV.PORT, () => {
-  console.log(`Server is running on port ${ENV.PORT}`);
-});
+const startServer = async () => {
+  try {
+    await connectDB();
+    app.listen(ENV.PORT, () => {
+      console.log(`Server is running on port ${ENV.PORT}`);
+    });
+  } catch (error) {
+    console.error("Error while the starting server",error);
+  }
+};
 
-
-
-
-
+startServer();
