8321984b211ca7de8d4b0f29d7b73c239a00ffaf
Yogendra Rajput <yogendra.leetcode123@gmail.com>
2025-11-28 19:11:52 +0530

    server side auth

---
+++ backend/src/controllers/chatController.js
@@ -0,0 +1,18 @@
+import { chatClient } from "../lib/stream.js";
+
+export async function getStreamToken(req, res) {
+  try {
+    //user clerk id for stream not mongodb _id=>it should match the id we have in the stream dashboard
+    const token = chatClient.createToken(req.user.clerkId);
+
+    res.status(200).json({
+      token,
+      userId: req.user.clerkId,
+      userName: req.user.name,
+      image: req.user.image,
+    });
+  } catch (error) {
+    console.error("Error in getStreamToken controller", error.message);
+    res.status(500).json({ msg: "Internal Server error" });
+  }
+}
