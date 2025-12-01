6fc7c873fb1dd36d31a7a7847cbd135e6daa6ce8
Yogendra Rajput <yogendra.leetcode123@gmail.com>
2025-12-01 18:09:47 +0530

    updating

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,10 +1,13 @@
 import axios from "axios";
+import { getToken } from "./getToken";
+const token=getToken() ||"";
 
-
-const axiosInstance=axios.create({
-    baseURL:import.meta.env.VITE_API_URL ,
-    withCredentials:true, //By adding this field browser will send the cookie to server automatically, on every single req
+const axiosInstance = axios.create({
+  headers: { Authorization: `Bearer ${token}` },
+  baseURL: import.meta.env.VITE_API_URL,
+  withCredentials: true, //By adding this field browser will send the cookie to server automatically, on every single req
 });
 
 export default axiosInstance;
+
 
