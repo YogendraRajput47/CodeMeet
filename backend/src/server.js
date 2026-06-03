d05364a25cf0a9d4816ee05e2c942c88b24f4b8c
YogendraRajput47 <yogendra.leetcode123@gmail.com>
2026-06-03 14:37:06 +0530

    Update CORS configuration to allow multiple origins

--- backend/src/server.js
+++ backend/src/server.js
@@ -20,7 +20,15 @@ const __dirname = path.resolve();
 // middleware
 app.use(express.json());
 // credentials:true meaning?? => server allows a browser to include cookies on request
-app.use(cors({ origin: ENV.CLIENT_URL, credentials: true }));
+app.use(
+  cors({
+    origin: [
+      "http://localhost:5173",
+      "https://code-meet-azure.vercel.app",
+    ],
+    credentials: true,
+  })
+);
 app.use(clerkMiddleware()); // this adds auth field to request object: req.auth()
 
 
